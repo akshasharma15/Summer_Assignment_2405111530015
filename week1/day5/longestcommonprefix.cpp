da94eb8f02fec77c44d77f2248a468dd14280441
akshasharma15 <akshasharma3999@gmail.com>
2026-06-05 17:26:07 +0530

    Update longestcommonprefix.cpp

--- week1/day5/longestcommonprefix.cpp
+++ week1/day5/longestcommonprefix.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result = "";
 
+        for(int i = 0; i < strs[0].size(); i++) {
+            char c = strs[0][i];
+
+            for(int j = 1; j < strs.size(); j++) {
+                if(i >= strs[j].size() || strs[j][i] != c) {
+                    return result;
+                }
+            }
+
+            result += c;
+        }
+
+        return result;
+        
+    }
+};
