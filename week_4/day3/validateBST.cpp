52654d2cb9fb05112f254c16a87537385a56b6e9
akshasharma15 <akshasharma3999@gmail.com>
2026-06-24 18:00:22 +0530

    Update validateBST.cpp

--- week_4/day3/validateBST.cpp
+++ week_4/day3/validateBST.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return valid(root, LONG_MIN, LONG_MAX);
+    }
 
+    bool valid(TreeNode* root, long minVal, long maxVal) {
+        if (root == nullptr) return true;
+
+        if (root->val <= minVal || root->val >= maxVal)
+            return false;
+
+        return valid(root->left, minVal, root->val) &&
+               valid(root->right, root->val, maxVal);
+    }
+};
